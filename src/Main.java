22b2125269258795085a4de11cb71e7ec2d71d13
“KhornMolika” <molikakhorn71@gmail.com>
2024-06-17 01:19:46 +0700

    remove unnecessary import

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import controller.CustomerController;
-import controller.OrderController;
-import controller.ProductController;
+
 import exception.ExceptionHandling;
-import model.dao.*;
-import model.entity.Customer;
-import model.entity.Order;
-import model.entity.Product;
-import model.service.CustomerServiceImpl;
 import view.View;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws ExceptionHandling {
